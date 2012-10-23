d37b5fa540e0a368a522f94d69256902958a377b
Braden Shepherdson <braden@chromium.org>
2012-10-23 15:07:06 -0400

    Now working as a Chrome app.
    I shouldn't have combined the background page JS code with mobile-spec's
    main.js. They're separated again.
    
    Navigation when you press the buttons is blowing away all the Chrome
    stuff, so it doesn't work right now.

--- main.js
+++ main.js
@@ -159,16 +159,5 @@ function init() {
     // document.addEventListener("touchmove", preventBehavior, false);
     document.addEventListener("deviceready", deviceInfo, true);
 
-    /**
-     * Listens for the app launching then creates the window.
-     * Ignores the provided window size.
-     *
-     * @see http://developer.chrome.com/trunk/apps/app.window.html
-     */
-    chrome.app.runtime.onLaunched.addListener(function() {
-      chrome.app.window.create('index.html', {
-        width: 244,
-        height: 380,
-      });
-    });
 }
+
