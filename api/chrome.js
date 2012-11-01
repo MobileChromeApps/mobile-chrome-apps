101c4be6d4231038afd0003c582cc3d041d68c75
Michal Mocny <mmocny@gmail.com>
2012-11-01 17:37:20 -0400

    Adding chrome.storage

--- api/chrome.js
+++ api/chrome.js
@@ -4,4 +4,5 @@ define('chrome', function(require) {
   // API modules export functions that expect this value as an argument and populate it with their part of the API.
   require('chrome.app');
   require('chrome.runtime');
+  require('chrome.storage');
 });
