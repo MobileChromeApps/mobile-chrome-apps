e21cf3cfc0872f2a54ff2acbdf86707aaa61270b
Andrew Grieve <agrieve@chromium.org>
2012-11-08 23:46:59 -0500

    Refactor runtime spec test to remove inline scripts.
    -Moved all the JS logic into the background page to avoid having to
    create more script files.
    -The spec now works on Chrome as well as on Cordova.
    -To have it work on chrome, you need to use cp -RL runtime runtime2, and
    then load runtime2 as an unpacked extension.

--- spec/runtime/background.js
+++ spec/runtime/background.js
@@ -4,10 +4,52 @@
  *
  * @see http://developer.chrome.com/trunk/apps/app.window.html
  */
+
+var wnd = null;
+var doc = null;
+
 chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('index.html', {
-    width: 244,
+    width: 320,
     height: 380,
+  }, function(w) {
+    wnd = w.contentWindow;
+    doc = wnd.document;
+    wnd.onload = onLoad;
   });
 });
+
+function onLoad() {
+  addButtonClickListeners();
+}
+
+function addButtonClickListeners() {
+  var buttons = doc.getElementsByClassName('btn');
+  for (var i = 0, btn; btn = buttons[i]; ++i) {
+    btn.onclick = window[btn.id];
+  }
+}
+
+// Check that chrome.app.runtime.onLaunched exists.
+function checkOnLaunched() {
+  doc.getElementById('status').innerHTML = chrome.app.runtime.onLaunched ? "onLaunched exists" : "onLaunched not found";
+};
+
+// Check that chrome.app.runtime.onSuspend exists.
+function checkOnSuspend() {
+  doc.getElementById('status').innerHTML = chrome.runtime.onSuspend ? "onSuspend exists" : "onSuspend not found";
+};
+
+// Attach a handler to onSuspend that will populate the status field when it gets called.
+function attachOnSuspend() {
+  var buttonTime = new Date();
+  chrome.runtime.onSuspend.addListener(function() {
+    var callbackTime = new Date();
+    doc.getElementById('status').innerHTML = 'onSuspend fired: ' + (callbackTime.getTime() - buttonTime.getTime()) + 'ms after button';
+  });
+}
+
+function backHome() {
+  wnd.location = '../chromeapp.html';
+}
 
