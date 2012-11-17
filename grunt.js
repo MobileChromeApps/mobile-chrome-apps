6ae2f9e1ca69cf5f1f7d506213a8041d22f3efcf
Andrew Grieve <agrieve@chromium.org>
2012-11-17 15:51:10 -0500

    Move grunt.js to top directory.

--- grunt.js
+++ grunt.js
@@ -5,31 +5,33 @@ module.exports = function(grunt) {
   grunt.initConfig({
     meta: {
       version: '0.1.0',
-      banner: '/*! Cordova for Chrome - v<%= meta.version %> - ' +
+      api_banner: '/*! Cordova for Chrome - v<%= meta.version %> - ' +
         '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
         '* http://github.com/MobileChromeApps/\n' +
         '* Copyright (c) <%= grunt.template.today("yyyy") %> ' +
         'Google Inc.; Licensed MIT */'
     },
     lint: {
-      files: ['grunt.js', 'chrome.js', 'chrome/**/*.js', 'helpers/**/*.js'] // prefix.js and suffix.js are deliberately excluded, they would never pass.
+      files: ['grunt.js', 'api/**/!(prefix|suffix).js']
+      // TODO(agrieve): Enable linting for spec.
+      // files: ['grunt.js', 'api/**/!(prefix|suffix).js', 'spec/**/!(jasmine*).js']
     },
     concat: {
-      dist: {
+      api: {
         // This should ensure that the top-level API gets included first, before any other files or subdirectories.
-        src: ['<banner:meta.banner>', 'prefix.js', 'chrome/**/*.js', 'helpers/**/*.js', 'chrome.js', 'suffix.js'],
-        dest: 'dist/chromeapi.js'
+        src: ['<banner:meta.api_banner>', 'api/prefix.js', 'api/chrome/**/*.js', 'api/helpers/**/*.js', 'api/chrome.js', 'api/suffix.js'],
+        dest: 'grunt_output/api/chromeapi.js'
       }
     },
     min: {
-      dist: {
-        src: ['<banner:meta.banner>', '<config:concat.dist.dest>'],
-        dest: 'dist/chromeapi.min.js'
+      api: {
+        src: ['<banner:meta.api_banner>', '<config:concat.api.dest>'],
+        dest: 'grunt_output/api/chromeapi.min.js'
       }
     },
     watch: {
-      files: ['<config:lint.files>', 'grunt.js', 'suffix.js', 'prefix.js', 'chrome.js'],
-      tasks: 'lint concat min'
+      files: ['<config:concat.api.src>'],
+      tasks: 'lint concat'
     },
     jshint: {
       options: {
