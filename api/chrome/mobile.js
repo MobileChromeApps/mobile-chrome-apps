98fb6f6e49522de1d469fe8be1b09723e6dad087
Andrew Grieve <agrieve@chromium.org>
2012-11-29 14:49:40 -0500

    Move start-up logic from chromeapp.html -> api/chrome/mobile.js

--- api/chrome/mobile.js
+++ api/chrome/mobile.js
@@ -3,12 +3,52 @@
 // found in the LICENSE file.
 
 define('chrome.mobile.impl', function(require, module) {
+  var chrome = window.chrome;
   var exports = module.exports;
+  var eventIframe = null;
 
-  exports.init = function(fgWindow, eventIframe) {
-    exports.fgWindow = fgWindow;
-    exports.bgWindow = eventIframe.contentWindow;
-    exports.eventIframe = eventIframe;
-    exports.bgWindow.chrome = window.chrome;
+  exports.init = function() {
+    function loadIframe(callback) {
+      var iframe = document.createElement('iframe');
+      iframe.src = 'chromebgpage.html';
+      iframe.style.display = 'none';
+      iframe.onload = function() {
+        iframe.onload = null;
+        eventIframe = iframe;
+        callback();
+      };
+      document.body.appendChild(iframe);
+    }
+
+    function loadBgPage() {
+      var manifestJson = chrome.runtime.getManifest();
+      var scripts = manifestJson.app.background.scripts;
+      var iframeDoc = eventIframe.contentDocument;
+      var numScriptsLoaded = 0;
+      function injectScript(path) {
+        var scriptNode = iframeDoc.createElement('script');
+        scriptNode.onload = afterScriptsLoaded;
+        scriptNode.src = path;
+        iframeDoc.body.appendChild(scriptNode);
+      }
+      function afterScriptsLoaded() {
+        this.onload = null;
+        if (++numScriptsLoaded == scripts.length) {
+          chrome.app.runtime.onLaunched.fire();
+        }
+      }
+      exports.fgWindow = window;
+      exports.bgWindow = eventIframe.contentWindow;
+      exports.eventIframe = eventIframe;
+      exports.bgWindow.chrome = chrome;
+
+      for (var i = 0, scriptPath; scriptPath = scripts[i]; ++i) {
+        injectScript(scriptPath);
+      }
+    }
+    // Self-destruct so that code in here can be GC'ed.
+    exports.init = null;
+    loadIframe(loadBgPage);
+
   };
 });
