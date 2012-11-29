be9bc006a0e6efed27af5a05e21dc76ee201f9c2
Andrew Grieve <agrieve@chromium.org>
2012-11-29 15:24:00 -0500

    Fix bootstrap logic to inject background scripts before onload.

--- api/chrome/mobile.js
+++ api/chrome/mobile.js
@@ -5,50 +5,41 @@
 define('chrome.mobile.impl', function(require, module) {
   var chrome = window.chrome;
   var exports = module.exports;
-  var eventIframe = null;
+
+  exports.fgWindow = window;
+  exports.bgWindow = null;
+  exports.eventIframe = null;
 
   exports.init = function() {
-    function loadIframe(callback) {
-      var iframe = document.createElement('iframe');
-      iframe.src = 'chromebgpage.html';
-      iframe.style.display = 'none';
-      iframe.onload = function() {
-        iframe.onload = null;
-        eventIframe = iframe;
-        callback();
-      };
-      document.body.appendChild(iframe);
-    }
-
-    function loadBgPage() {
-      var manifestJson = chrome.runtime.getManifest();
-      var scripts = manifestJson.app.background.scripts;
-      var iframeDoc = eventIframe.contentDocument;
-      var numScriptsLoaded = 0;
-      function injectScript(path) {
-        var scriptNode = iframeDoc.createElement('script');
-        scriptNode.onload = afterScriptsLoaded;
-        scriptNode.src = path;
-        iframeDoc.body.appendChild(scriptNode);
-      }
-      function afterScriptsLoaded() {
-        this.onload = null;
-        if (++numScriptsLoaded == scripts.length) {
-          chrome.app.runtime.onLaunched.fire();
-        }
-      }
-      exports.fgWindow = window;
-      exports.bgWindow = eventIframe.contentWindow;
-      exports.eventIframe = eventIframe;
-      exports.bgWindow.chrome = chrome;
-
-      for (var i = 0, scriptPath; scriptPath = scripts[i]; ++i) {
-        injectScript(scriptPath);
-      }
-    }
     // Self-destruct so that code in here can be GC'ed.
     exports.init = null;
-    loadIframe(loadBgPage);
+    var iframe = document.createElement('iframe');
+    iframe.src = 'chromebgpage.html';
+    iframe.style.display = 'none';
+    exports.eventIframe = iframe;
+    document.body.appendChild(iframe);
+  };
 
+  exports.bgInit = function(bgWnd) {
+    // Self-destruct so that code in here can be GC'ed.
+    exports.bgInit = null;
+    exports.bgWindow = bgWnd;
+    bgWnd.chrome = chrome;
+
+    function onLoad() {
+      bgWnd.removeEventListener('load', onLoad, false);
+      setTimeout(function() {
+        chrome.app.runtime.onLaunched.fire();
+      }, 0);
+    }
+    bgWnd.addEventListener('load', onLoad, false);
+
+    var manifestJson = chrome.runtime.getManifest();
+    var scripts = manifestJson.app.background.scripts;
+    var toWrite = '';
+    for (var i = 0, src; src = scripts[i]; ++i) {
+      toWrite += '<script src="' + encodeURI(src) + '"></sc' + 'ript>\n';
+    }
+    bgWnd.document.write(toWrite);
   };
 });
