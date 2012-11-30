5bc765f8a4b775fb7774b3a9e59f31a8858ba136
Andrew Grieve <agrieve@chromium.org>
2012-11-30 11:27:05 -0500

    Add spec tests for all chrome.runtime APIs.
    They currently fail in Cordova.

--- spec/spec_common/jasmine_helper.js
+++ spec/spec_common/jasmine_helper.js
@@ -2,3 +2,16 @@
 function chromeSpec(func) {
   chromeSpecs[!!window.runningInBg].push(func);
 }
+
+function itShouldHaveAnEvent(obj, eventName) {
+  it('should have an event called ' + eventName, function() {
+    expect(obj[eventName].constructor == chrome.Event).toBe(true);
+  });
+}
+
+function itShouldHaveAPropertyOfType(obj, propName, typeName) {
+  it('should have a "' + propName + '" ' + typeName, function() {
+    expect(typeof obj[propName]).toBe(typeName);
+  });
+}
+
--- spec/test.chrome.runtime.js
+++ spec/test.chrome.runtime.js
@@ -4,13 +4,47 @@
 
 chromeSpec(function(runningInBackground) {
   describe('chrome.runtime', function() {
-    it('should have onSuspend exist', function() {
-      expect(chrome.runtime.onSuspend).not.toBeUndefined();
-    });
     it('getManifest() should have a name that is a string', function() {
       var manifest = chrome.runtime.getManifest();
       expect(typeof manifest.name).toBe('string');
     });
+    it('getBackgroundPage() should throw when args are invalid', function() {
+      expect(function() {chrome.runtime.getBackgroundPage();}).toThrow();
+      expect(function() {chrome.runtime.getBackgroundPage(1);}).toThrow();
+    });
+    it('getBackgroundPage() should provide a window object asynchronously.', function() {
+      var bgPage = null;
+      chrome.runtime.getBackgroundPage(function(wnd) {
+        bgPage = wnd;
+        if (runningInBackground) {
+          expect(window == bgPage).toBe(true, 'window should == bgPage');
+        } else {
+          expect(window == bgPage).toBe(false, 'window should != bgPage');
+        }
+      });
+      expect(bgPage).toBeNull();
+      waitsFor(function() {
+        return !!bgPage;
+      });
+    });
+    it('getURL() should throw when not args are invalid.', function() {
+      expect(function() {chrome.runtime.getURL();}).toThrow();
+      expect(function() {chrome.runtime.getURL(3);}).toThrow();
+    });
+    it('getURL() should work', function() {
+      var prefix = location.href.replace(/[^\/]*$/, '');
+      expect(chrome.runtime.getURL('')).toBe(prefix);
+      expect(chrome.runtime.getURL('b')).toBe(prefix + 'b');
+      expect(chrome.runtime.getURL('/b')).toBe(prefix + 'b');
+    });
+    itShouldHaveAnEvent(chrome.runtime, 'onInstalled');
+    itShouldHaveAnEvent(chrome.runtime, 'onStartup');
+    itShouldHaveAnEvent(chrome.runtime, 'onSuspend');
+    itShouldHaveAnEvent(chrome.runtime, 'onSuspendCanceled');
+    itShouldHaveAnEvent(chrome.runtime, 'onUpdateAvailable');
+    itShouldHaveAPropertyOfType(chrome.runtime, 'id', 'string');
+    itShouldHaveAPropertyOfType(chrome.runtime, 'reload', 'function');
+    itShouldHaveAPropertyOfType(chrome.runtime, 'requestUpdateCheck', 'function');
   });
 });
 
