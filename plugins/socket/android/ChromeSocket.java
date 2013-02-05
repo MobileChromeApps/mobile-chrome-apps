96b7c836d028bc89a21f8b6e5bb6d64a722b3a83
Braden Shepherdson <braden@chromium.org>
2013-02-05 14:17:40 -0500

    Implement listen() and accept() for server sockets.

--- plugins/socket/android/ChromeSocket.java
+++ plugins/socket/android/ChromeSocket.java
@@ -5,7 +5,10 @@
 package org.apache.cordova;
 
 import java.io.IOException;
+import java.io.InterruptedIOException;
+import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
 import java.net.UnknownHostException;
 import java.util.HashMap;
 import java.util.Map;
@@ -17,7 +20,6 @@ import org.apache.cordova.api.CallbackContext;
 import org.apache.cordova.api.CordovaPlugin;
 import org.apache.cordova.api.PluginResult;
 import org.json.JSONException;
-import org.json.JSONObject;
 
 import android.util.Log;
 import android.util.Pair;
@@ -26,8 +28,8 @@ public class ChromeSocket extends CordovaPlugin {
 
     private static final String LOG_TAG = "ChromeSocket";
 
-    Map<Integer, SocketData> sockets = new HashMap<Integer, SocketData>();
-    int nextSocket = 1;
+    private static Map<Integer, SocketData> sockets = new HashMap<Integer, SocketData>();
+    private static int nextSocket = 1;
 
     @Override
     public boolean execute(String action, CordovaArgs args, final CallbackContext callbackContext) throws JSONException {
@@ -49,6 +51,12 @@ public class ChromeSocket extends CordovaPlugin {
         } else if ("destroy".equals(action)) {
             destroy(args, callbackContext);
             return true;
+        } else if ("listen".equals(action)) {
+            listen(args, callbackContext);
+            return true;
+        } else if ("accept".equals(action)) {
+            accept(args, callbackContext);
+            return true;
         }
         return false;
     }
@@ -58,14 +66,18 @@ public class ChromeSocket extends CordovaPlugin {
         String socketType = args.getString(0);
         if (socketType.equals("tcp")) {
             SocketData sd = new SocketData(SocketData.Type.TCP);
-            sockets.put(Integer.valueOf(nextSocket), sd);
-            callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, nextSocket));
-            nextSocket++;
+            int id = addSocket(sd);
+            callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, id));
         } else if (socketType.equals("udp")) {
             Log.w(LOG_TAG, "UDP is not currently supported");
         } else {
             Log.e(LOG_TAG, "Unknown socket type: " + socketType);
         }
+    }
+
+    private static int addSocket(SocketData sd) {
+        sockets.put(Integer.valueOf(nextSocket), sd);
+        return nextSocket++;
     }
 
     private void connect(CordovaArgs args, final CallbackContext callbackContext) throws JSONException {
@@ -139,25 +151,65 @@ public class ChromeSocket extends CordovaPlugin {
     }
 
 
+    private void listen(CordovaArgs args, final CallbackContext callbackContext) throws JSONException {
+        int socketId = args.getInt(0);
+
+        SocketData sd = sockets.get(Integer.valueOf(socketId));
+        if (sd == null) {
+            Log.e(LOG_TAG, "No socket with socketId " + socketId);
+            return;
+        }
+
+        String address = args.getString(1);
+        int port = args.getInt(2);
+        int backlog = args.getInt(3);
+        sd.listen(address, port, backlog);
+        callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, 1));
+    }
+
+    private void accept(CordovaArgs args, final CallbackContext callbackContext) throws JSONException {
+        int socketId = args.getInt(0);
+
+        SocketData sd = sockets.get(Integer.valueOf(socketId));
+        if (sd == null) {
+            Log.e(LOG_TAG, "No socket with socketId " + socketId);
+            return;
+        }
+
+        sd.accept(callbackContext);
+    }
+
+
     private static class SocketData {
         Socket socket;
+        ServerSocket serverSocket;
 
         public enum Type { TCP, UDP; }
         private Type type;
 
         BlockingQueue<Pair<Integer, CallbackContext>> readQueue;
-        private SocketThread readThread;
+        private ReadThread readThread;
+
+        BlockingQueue<CallbackContext> acceptQueue;
+        private AcceptThread acceptThread;
+
+        private boolean isServer = false;
 
         public SocketData(Type type) {
             this.type = type;
         }
 
+        public SocketData(Socket incoming) {
+            this.type = Type.TCP;
+            socket = incoming;
+            init();
+        }
+
         public int connect(String address, int port) {
+            if (isServer) return 1; // error
             try {
                 socket = new Socket(address, port);
-                readQueue = new LinkedBlockingQueue<Pair<Integer, CallbackContext>>();
-                readThread = new SocketThread();
-                readThread.start();
+                init();
             } catch(UnknownHostException uhe) {
                 return 1; // error
             } catch(IOException ioe) {
@@ -166,8 +218,14 @@ public class ChromeSocket extends CordovaPlugin {
             return 0; // success
         }
 
+        private void init() {
+            readQueue = new LinkedBlockingQueue<Pair<Integer, CallbackContext>>();
+            readThread = new ReadThread();
+            readThread.start();
+        }
+
         public int write(byte[] data) throws JSONException {
-            if (socket == null) return -1;
+            if (socket == null || isServer) return -1;
 
             int bytesWritten = 0;
             try {
@@ -181,6 +239,11 @@ public class ChromeSocket extends CordovaPlugin {
         }
 
         public void read(int bufferLength, CallbackContext context) {
+            if (isServer) {
+                context.error("read() is not allowed on server sockets");
+                return;
+            }
+
             synchronized(readQueue) {
                 try {
 					readQueue.put(new Pair<Integer, CallbackContext>(Integer.valueOf(bufferLength), context));
@@ -192,21 +255,59 @@ public class ChromeSocket extends CordovaPlugin {
 
         public void disconnect() {
             try {
-                readThread.setStop();
-            	socket.close();
+                if (isServer) {
+                    acceptThread.setStop();
+                    serverSocket.close();
+                } else {
+                    readThread.setStop();
+                    socket.close();
+                }
             } catch (IOException ioe) {
             }
             socket = null;
+            serverSocket = null;
         }
 
         public void destroy() {
-            if (socket != null) {
+            if (socket != null || serverSocket != null) {
                 disconnect();
             }
         }
 
 
-        private class SocketThread extends Thread {
+        public void listen(String address, int port, int backlog) {
+            if (type != Type.TCP) return;
+            isServer = true;
+
+            try {
+                serverSocket = new ServerSocket(port, backlog);
+            } catch (IOException ioe) {
+                Log.e(LOG_TAG, "Error creating server socket", ioe);
+            }
+        }
+
+        public void accept(CallbackContext context) {
+            if (!isServer) {
+                context.error("accept() is not supported on client sockets. Call listen() first.");
+                return;
+            }
+
+            if (acceptQueue == null && acceptThread == null) {
+                acceptQueue = new LinkedBlockingQueue<CallbackContext>();
+                acceptThread = new AcceptThread();
+                acceptThread.start();
+            }
+
+            synchronized(acceptQueue) {
+                try {
+                    acceptQueue.put(context);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        private class ReadThread extends Thread {
             private boolean doStop = false;
 
             public void run() {
@@ -255,6 +356,50 @@ public class ChromeSocket extends CordovaPlugin {
             public void setStop() {
                 doStop = true;
             }
-        } // SocketThread
+        } // ReadThread
+
+        private class AcceptThread extends Thread {
+            private boolean doStop = false;
+
+            public void run() {
+                try {
+                    serverSocket.setSoTimeout(120000); // 120 seconds
+                } catch (SocketException se) {
+                    Log.w(LOG_TAG, "Failed to set timeout for server socket", se);
+                }
+
+                while(!doStop) {
+                    try {
+                        CallbackContext context = null;
+                        while (!doStop && context == null) {
+                            context = SocketData.this.acceptQueue.poll(120L, TimeUnit.SECONDS);
+                        }
+                        if (doStop) return;
+
+                        Socket incoming = null;
+                        while (!doStop && incoming == null) {
+                            try {
+                                incoming = serverSocket.accept();
+                            } catch (InterruptedIOException iioe) {
+                                // Just silently try again.
+                            } catch (IOException ioe) {
+                                // Just silently try again.
+                            }
+                        }
+                        if (doStop) return;
+
+                        SocketData sd = new SocketData(incoming);
+                        int id = ChromeSocket.addSocket(sd);
+                        context.sendPluginResult(new PluginResult(PluginResult.Status.OK, id));
+                    } catch (InterruptedException ie) {
+                        Log.w(LOG_TAG, "Thread interrupted", ie);
+                    }
+                }
+            } // run()
+
+            public void setStop() {
+                doStop = true;
+            }
+        } // AcceptThread
     } // SocketData
 }
