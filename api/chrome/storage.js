0cdf2763c284eb116f090d7c01c265e79ecdabf1
Shravan Narayan <shravanrn@dhcp-172-23-180-146.wat.corp.google.com>
2013-02-25 15:27:28 -0500

    chrome.storage implementation for android

--- api/chrome/storage.js
+++ api/chrome/storage.js
@@ -4,7 +4,13 @@
 
 define('chrome.storage', function(require, module) {
 
-  function jsonReplacer(key) {
+var exports = module.exports;
+
+function StorageArea(syncStorage) {
+    this._sync = syncStorage;
+}
+
+StorageArea.prototype._jsonReplacer = function(key) {
     // Don't use the value passed in since it has already gone through toJSON().
     var value = this[key];
     // Refer to:
@@ -16,111 +22,103 @@ define('chrome.storage', function(require, module) {
       }
     }
     return value;
-  }
+};
 
-  function StorageArea(namespaceChar) {
-    this._namespace = '_%_' + namespaceChar;
-  }
-
-  StorageArea.prototype.getBytesInUse = unsupportedApi('StorageArea.getBytesInUse');
-
-  StorageArea.prototype.clear = function() {
-    var toRemove = [];
-    for (var i = 0; i < localStorage.length; i++) {
-      var k = localStorage.key(i);
-      if (k.slice(0, 4) == this._namespace) {
-        toRemove.push(k);
-      }
+StorageArea.prototype._scrubValues = function(o) {
+    if (typeof o != 'undefined') {
+        var t = JSON.stringify(o, this._jsonReplacer);
+        return JSON.parse(t);
     }
-    toRemove.forEach(localStorage.removeItem, localStorage);
-  };
+};
 
-  StorageArea.prototype.set = function(items) {
-    for (var key in items) {
-      if (items.hasOwnProperty(key)) {
-        if (typeof items[key] != 'undefined') {
-          var value = JSON.stringify(items[key], jsonReplacer);
-          localStorage.setItem(this._namespace + key, value);
-        }
-      }
+StorageArea.prototype.get = function(keys, callback) {
+    if (typeof keys == 'function') {
+        callback = keys;
+        keys = null;
+    } else if (typeof keys === 'string') {
+        keys = [keys];
     }
-  };
+    var win = callback && function(args) {
+        callback(args);
+    };
+    var fail = callback && function() {
+        callback();
+    };
+    var param = this._scrubValues(keys);
+    cordova.exec(win, fail, 'ChromeStorage', 'get', [this._sync, param]);
+};
 
-  StorageArea.prototype.remove = function(keys) {
-    if (typeof keys == 'string') {
-      keys = [keys];
+StorageArea.prototype.getBytesInUse = function(keys, callback) {
+    if (typeof keys == 'function') {
+        callback = keys;
+        keys = null;
+    } else if (typeof keys === 'string') {
+        keys = [keys];
     }
-    for (var i = 0; i < keys.length; ++i) {
-      localStorage.removeItem(this._namespace + keys[i]);
+    var win = callback && function(bytes) {
+        callback(bytes);
+    };
+    var fail = callback && function() {
+        callback(-1);
+    };
+    var param = this._scrubValues(keys);
+    cordova.exec(win, fail, 'ChromeStorage', 'getBytesInUse', [this._sync, param]);
+};
+
+StorageArea.prototype.set = function(keyVals, callback) {
+    if (typeof keyVals == 'function') {
+        callback = keyVals;
+        keyVals = null;
     }
-  };
+    var win = callback && function() {
+        callback(0);
+    };
+    var fail = callback && function() {
+        callback(-1);
+    };
+    var param = this._scrubValues(keyVals);
+    cordova.exec(win, fail, 'ChromeStorage', 'set', [this._sync, param]);
+};
 
-  StorageArea.prototype.get = function(items, callback) {
-    var ret = {};
-
-    if (typeof items == 'function') {
-      callback = items;
-      items = null;
+StorageArea.prototype.remove = function(keys, callback) {
+    if (typeof keys == 'function') {
+        callback = keys;
+        keys = null;
+    } else if (typeof keys === 'string') {
+        keys = [keys];
     }
-    if (typeof callback != 'function') {
-      throw 'callback must be a function';
-    }
+    var win = callback && function() {
+        callback(0);
+    };
+    var fail = callback && function() {
+        callback(-1);
+    };
+    var param = this._scrubValues(keys);
+    cordova.exec(win, fail, 'ChromeStorage', 'remove', [this._sync, param]);
+};
 
-    var namespace = this._namespace;
-    function getLocalStorageValuesForKeys(keys) {
-      var ret = {};
-      keys.forEach(function(key) {
-        var item = localStorage.getItem(namespace + key);
-        if (item !== null) {
-          ret[key] = JSON.parse(item);
-        }
-      });
-      return ret;
-    }
+StorageArea.prototype.clear = function(callback) {
+    var win = callback && function() {
+        callback(0);
+    };
+    var fail = callback && function() {
+        callback(-1);
+    };
+    cordova.exec(win, fail, 'ChromeStorage', 'clear', [this._sync]);
+};
 
+var local = new StorageArea(false);
+local.QUOTA_BYTES = 5242880;
+var sync = new StorageArea(true);
+sync.MAX_ITEMS = 512;
+sync.MAX_WRITE_OPERATIONS_PER_HOUR = 1000;
+sync.QUOTA_BYTES_PER_ITEM = 4096;
+sync.MAX_SUSTAINED_WRITE_OPERATIONS_PER_MINUTE = 10;
+sync.QUOTA_BYTES = 102400;
 
-    if (items == null) {
-      var keys = [];
-      for (var i = 0; i < localStorage.length; i++) {
-        keys.push(localStorage.key(i).slice(4));
-      }
-      ret = getLocalStorageValuesForKeys(keys);
-    } else if (typeof items == 'string') {
-      ret = getLocalStorageValuesForKeys([items]);
-    } else if (Array.isArray(items)) {
-      ret = getLocalStorageValuesForKeys(items);
-    } else {
-      ret = items; // assign defaults
-      var o = getLocalStorageValuesForKeys(Object.keys(items));
-      Object.keys(o).forEach(function(key) {
-          ret[key] = o[key];
-      });
-    }
-    callback(ret);
-  };
-/*
-  function StorageChange(oldValue, newValue) {
-    this.oldValue = oldValue;
-    this.newValue = newValue;
-  }
-*/
-  var local = new StorageArea('l');
-  local.QUOTA_BYTES = 5242880;
+exports.local = local;
+exports.sync = sync;
 
-  var sync = new StorageArea('s');
-  sync.MAX_ITEMS = 512;
-  sync.MAX_WRITE_OPERATIONS_PER_HOUR = 1000;
-  sync.QUOTA_BYTES_PER_ITEM = 4096;
-  sync.MAX_SUSTAINED_WRITE_OPERATIONS_PER_MINUTE = 10;
-  sync.QUOTA_BYTES = 102400;
-
-  var exports = module.exports;
-  exports.local = local;
-  exports.sync = sync;
-
-  // TODO(mmocny): Hook up this event so it actually gets called(?)
-  var Event = require('chrome.Event');
-  exports.onChanged = new Event('onChanged');
-  //chrome.storage.onChanged.addListener(function(object changes, string areaName) {...});
-
+var Event = require('chrome.Event');
+exports.onChanged = new Event('onChanged');
 });
---
+++ plugins/storage/android/ChromeStorage.java
@@ -0,0 +1,243 @@
+package org.apache.cordova;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.StreamCorruptedException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import org.apache.cordova.api.CallbackContext;
+import org.apache.cordova.api.CordovaPlugin;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.content.Context;
+import android.util.Log;
+
+public class ChromeStorage extends CordovaPlugin {
+
+    private static final String LOG_TAG = "ChromeStorage";
+    private ExecutorService executorService = null;
+
+    public ChromeStorage() {
+        executorService = Executors.newSingleThreadExecutor();
+    }
+
+    @Override
+    public boolean execute(String action, CordovaArgs args, final CallbackContext callbackContext) throws JSONException {
+        if ("get".equals(action)) {
+            get(args, callbackContext);
+            return true;
+        } else if ("getBytesInUse".equals(action)) {
+            getBytesInUse(args, callbackContext);
+            return true;
+        } else if ("set".equals(action)) {
+            set(args, callbackContext);
+            return true;
+        } else if ("remove".equals(action)) {
+            remove(args, callbackContext);
+            return true;
+        } else if ("clear".equals(action)) {
+            clear(args, callbackContext);
+            return true;
+        }
+
+        return false;
+    }
+
+    private static String getStorageFile(boolean sync) {
+        return sync? "__chromestorage_sync" : "__chromestorage";
+    }
+
+    private JSONObject getStorage(boolean sync) throws StreamCorruptedException, IOException, ClassNotFoundException, JSONException {
+        Context context = this.cordova.getActivity();
+        File file = new File(context.getFilesDir(), getStorageFile(sync));
+        JSONObject oldMap = new JSONObject();
+
+        if(file.exists()) {
+            FileInputStream fis = null;
+            try {
+                fis = new FileInputStream(file);
+                byte[] bytes = new byte[(int) file.length()];
+                fis.read(bytes);
+                String content = (new String(bytes)).trim();
+                if (!content.isEmpty()) {
+                    oldMap = new JSONObject(content);
+                }
+            } catch (FileNotFoundException e) {
+                //Suppress the file not found exception
+            } finally {
+                try { fis.close(); } catch (Exception e1) { }
+            }
+        }
+        return oldMap;
+    }
+
+    private void setStorage(boolean sync, JSONObject map) throws IOException {
+        Context context = this.cordova.getActivity();
+        FileOutputStream fos = context.openFileOutput(getStorageFile(sync), Context.MODE_PRIVATE);
+        fos.write(map.toString().getBytes());
+        fos.close();
+    }
+
+    private JSONObject getStoredValuesForKeys(CordovaArgs args, boolean useDefaultValues) {
+        JSONObject ret = new JSONObject();
+        try {
+            boolean sync = args.getBoolean(0);
+            JSONObject jsonObject = (JSONObject) args.optJSONObject(1);
+            JSONArray jsonArray = args.optJSONArray(1);
+            boolean isNull = args.isNull(1);
+            List<String> keys = new ArrayList<String>();
+
+            if (jsonObject != null) {
+                keys = JSONUtils.toStringList(jsonObject.names());
+                // Ensure default values of keys are maintained
+                if (useDefaultValues) {
+                    ret = jsonObject;
+                }
+            } else if (jsonArray != null) {
+                keys = JSONUtils.toStringList(jsonArray);
+            } else if (isNull) {
+                keys = null;
+            }
+
+            if (keys != null && keys.isEmpty()) {
+                ret = new JSONObject();
+            } else {
+                JSONObject storage = getStorage(sync);
+
+                if (keys == null) {
+                    // return the whole storage if the key given is null
+                    ret = storage;
+                } else {
+                    // return the storage for the keys specified
+                    for (String key : keys)    {
+                        if (storage.has(key)) {
+                            Object value = storage.get(key);
+                            ret.put(key, value);
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            Log.e(LOG_TAG, "Could not retrieve storage", e);
+            ret = null;
+        }
+
+        return ret;
+    }
+
+    private void get(final CordovaArgs args, final CallbackContext callbackContext) {
+        executorService.execute(new Runnable() {
+            @Override
+            public void run() {
+                JSONObject storage = getStoredValuesForKeys(args, /*useDefaultValues*/ true);
+
+                if (storage == null) {
+                    callbackContext.error("Could not retrieve storage");
+                } else {
+                    callbackContext.success(storage);
+                }
+            }
+        });
+    }
+
+    private void getBytesInUse(final CordovaArgs args, final CallbackContext callbackContext) {
+        executorService.execute(new Runnable() {
+            @Override
+            public void run() {
+                //Don't use default values as the keys that don't have values in storage don't affect size
+                JSONObject storage = getStoredValuesForKeys(args, /*useDefaultValues*/ false);
+        
+                if (storage == null) {
+                    callbackContext.error("Could not retrieve storage");
+                } else {
+                    callbackContext.success(storage.toString().getBytes().length);
+                }
+            }
+        });
+    }
+
+    private void set(final CordovaArgs args, final CallbackContext callbackContext) {
+        executorService.execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    boolean sync = args.getBoolean(0);
+                    JSONObject jsonObject = (JSONObject) args.getJSONObject(1);
+                    JSONArray keyArray = jsonObject.names();
+
+                    if (keyArray != null) {
+                        List<String> keys = JSONUtils.toStringList(keyArray);
+                        JSONObject storage = getStorage(sync);
+                        for (String key : keys) {
+                            storage.put(key, jsonObject.get(key));
+                        }
+                        setStorage(sync, storage);
+                    }
+                    callbackContext.success();
+                } catch (Exception e) {
+                    Log.e(LOG_TAG, "Could not update storage", e);
+                    callbackContext.error("Could not update storage");
+                }
+            }
+        });
+    }
+
+    private void remove(final CordovaArgs args, final CallbackContext callbackContext) {
+        executorService.execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    boolean sync = args.getBoolean(0);
+                    JSONObject jsonObject = (JSONObject) args.optJSONObject(1);
+                    JSONArray jsonArray = args.optJSONArray(1);
+                    boolean isNull = args.isNull(1);
+                    List<String> keys = new ArrayList<String>();
+
+                    if (jsonObject != null) {
+                        keys = JSONUtils.toStringList(jsonObject.names());
+                    } else if (jsonArray != null) {
+                        keys = JSONUtils.toStringList(jsonArray);
+                    } else if (isNull) {
+                        keys = null;
+                    }
+
+                    if (keys != null && !keys.isEmpty()) {
+                        JSONObject storage = getStorage(sync);
+                        for(String key : keys) {
+                            storage.remove(key);
+                        }
+                        setStorage(sync, storage);
+                    }
+                    callbackContext.success();
+                } catch (Exception e) {
+                    Log.e(LOG_TAG, "Could not update storage", e);
+                    callbackContext.error("Could not update storage");
+                }
+            }
+        });
+    }
+
+    private void clear(final CordovaArgs args, final CallbackContext callbackContext) {
+        executorService.execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    boolean sync = args.getBoolean(0);
+                    setStorage(sync, new JSONObject());
+                    callbackContext.success();
+                } catch (Exception e) {
+                    Log.e(LOG_TAG, "Could not clear storage", e);
+                    callbackContext.error("Could not update storage");
+                }
+            }
+        });
+    }
+}
