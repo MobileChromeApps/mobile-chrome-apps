86feef00aa5d762f3ffcc3621a2466c5874adb44
Braden Shepherdson <braden.shepherdson@gmail.com>
2013-02-25 14:23:23 -0500

    Modernize the main Chrome API Javascript files and make into a plugin
    Includes splitting chrome.Event into its own module (plugins/common)

---
+++ plugins/core/api/mobile.js
@@ -0,0 +1,58 @@
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+var chrome = window.chrome;
+var exports = module.exports;
+
+exports.fgWindow = window;
+exports.bgWindow = null;
+exports.eventIframe = null;
+
+function createBgChrome() {
+  return {
+    __proto__: chrome,
+    app: {
+      __proto__: chrome.app,
+      window: {
+        __proto__: chrome.app.window,
+        current: function() { return null; }
+      }
+    }
+  };
+}
+
+exports.init = function() {
+  // Self-destruct so that code in here can be GC'ed.
+  exports.init = null;
+  var iframe = document.createElement('iframe');
+  iframe.src = 'chromebgpage.html';
+  iframe.style.display = 'none';
+  exports.eventIframe = iframe;
+  document.body.appendChild(iframe);
+};
+
+exports.bgInit = function(bgWnd) {
+  // Self-destruct so that code in here can be GC'ed.
+  exports.bgInit = null;
+  exports.bgWindow = bgWnd;
+  bgWnd.chrome = createBgChrome();
+  bgWnd.cordova = cordova;
+  exports.fgWindow.opener = exports.bgWindow;
+
+  function onLoad() {
+    bgWnd.removeEventListener('load', onLoad, false);
+    setTimeout(function() {
+      chrome.app.runtime.onLaunched.fire();
+    }, 0);
+  }
+  bgWnd.addEventListener('load', onLoad, false);
+
+  var manifestJson = chrome.runtime.getManifest();
+  var scripts = manifestJson.app.background.scripts;
+  var toWrite = '';
+  for (var i = 0, src; src = scripts[i]; ++i) {
+    toWrite += '<script src="' + encodeURI(src) + '"></sc' + 'ript>\n';
+  }
+  bgWnd.document.write(toWrite);
+};
--- plugins/core/app/runtime.js
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-define('chrome.app.runtime', function(require, module) {
-  var Event = require('chrome.Event');
-  var exports = module.exports;
-  exports.onLaunched = new Event('onLaunched');
-});
