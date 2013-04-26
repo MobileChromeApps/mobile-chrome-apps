742a2fa7150a3eec49d92c6c5c2e26547ce6ac01
Michal Mocny <mmocny@gmail.com>
2013-04-26 15:22:07 -0400

    socket tests pass

--- spec/www/test.chrome.socket.js
+++ spec/www/test.chrome.socket.js
@@ -36,8 +36,9 @@ chromeSpec('chrome.socket', function(runningInBackground) {
     itWaitsForDone('port is available (sanity test)', function(done) {
       chrome.socket.create('tcp', function(createInfo) {
         chrome.socket.listen(createInfo.socketId, addr, port, function(listenResult) {
-          expect(listenResult).toEqual(0);
+          chrome.socket.disconnect(createInfo.socketId);
           chrome.socket.destroy(createInfo.socketId);
+          expect(listenResult).toEqual(0);
           done();
         });
       });
