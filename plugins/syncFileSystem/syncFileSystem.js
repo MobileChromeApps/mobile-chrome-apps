8087492a9ca11ec111f6e87a43613baa6400ebf3
Max Woghiren <max.woghiren@gmail.com>
2013-04-29 12:27:32 -0400

    Added sync functionality to FileWriter.write.
    Also, if the given file already exists on Drive, it is overwritten.  Drive supports multiple files with the same name, and inserting a file to Drive creates a new file by default, so this needs to be explicitly handled.
    
    Also corrected a comment.

--- plugins/syncFileSystem/syncFileSystem.js
+++ plugins/syncFileSystem/syncFileSystem.js
@@ -12,12 +12,6 @@ var _tokenString;
 // When we create or get the app's syncable Drive directory, we store its id here.
 var _syncableDirectoryId;
 
-// This function overrides the necessary functions on a given FileEntry to enable syncability.
-// It also uploads the associated file to Drive.
-function enableSyncabilityForFileEntry(fileEntry) {
-    // TODO(maxw): Update the given FileEntry's functions for syncability.
-}
-
 // This function overrides the necessary functions on a given DirectoryEntry to enable syncability.
 function enableSyncabilityForDirectoryEntry(directoryEntry) {
     directoryEntry.getFile = function(path, options, successCallback, errorCallback) {
@@ -36,6 +30,42 @@ function enableSyncabilityForDirectoryEntry(directoryEntry) {
 		// Call the original function.  The augmented success callback will take care of the syncability addition work.
         DirectoryEntry.prototype.getFile.call(directoryEntry, path, options, augmentedSuccessCallback, errorCallback);
     };
+}
+
+// This function overrides the necessary functions on a given FileEntry to enable syncability.
+// It also uploads the associated file to Drive.
+function enableSyncabilityForFileEntry(fileEntry) {
+	fileEntry.createWriter = function(successCallback, errorCallback) {
+		var augmentedSuccessCallback = function(fileWriter) {
+			enableSyncabilityForFileWriter(fileWriter, fileEntry);
+			if (successCallback) {
+				successCallback(fileWriter);
+			}
+		};
+
+		// Call the original function.  The augmented success callback will take care of the syncability addition work.
+        FileEntry.prototype.createWriter.call(fileEntry, augmentedSuccessCallback, errorCallback);
+	};
+}
+
+// This function overrides the necessary functions on a given FileWriter to enable syncability.
+function enableSyncabilityForFileWriter(fileWriter, fileEntry) {
+	fileWriter.write = function(data) {
+		// We want to augment the `onwrite` and `onwriteend` listeners to add syncing.
+		// TODO(maxw): Augment onwriteend.
+		if (fileWriter.onwrite) {
+			var originalOnwrite = fileWriter.onwrite;
+			fileWriter.onwrite = function(evt) {
+				var onSyncFileSuccess = function() {
+					originalOnwrite(evt);
+				};
+				syncFile(fileEntry, onSyncFileSuccess);
+			};
+		}
+
+		// Call the original function.  The augmented success callback will take care of the syncability addition work.
+        FileWriter.prototype.write.call(fileWriter, data);
+	};
 }
 
 // This function creates a directory on the user's Drive.
@@ -81,57 +111,66 @@ function syncDirectory(directoryEntry, callback) {
 
 // This function uploads a file to Drive.
 function syncFile(fileEntry, callback) {
-    var onFileSuccess = function(file) {
-        // Read the file and send its contents.
-        var fileReader = new FileReader();
-        fileReader.onloadend = function(evt) {
-            // Create the data to send.
-            var metadata = { title: fileEntry.name,
-                             parents: [{ id: _syncableDirectoryId }] };
-            var boundary = '2718281828459045';
-            var body = [];
-            body.push('--' + boundary);
-            body.push('Content-Type: application/json');
-            body.push('');
-            body.push(JSON.stringify(metadata));
-            body.push('');
-            body.push('--' + boundary);
-            // TODO(maxw): Use the correct content type.
-            body.push('Content-Type: text/plain');
-            body.push('');
-            body.push(fileReader.result);
-            body.push('');
-            body.push('--' + boundary + '--');
-            var bodyString = body.join('\r\n');
+	var onGetFileIdSuccess = function(fileId) {
+		var onFileSuccess = function(file) {
+			// Read the file and send its contents.
+		    var fileReader = new FileReader();
+		    fileReader.onloadend = function(evt) {
+		        // Create the data to send.
+		        var metadata = { title: fileEntry.name,
+		                         parents: [{ id: _syncableDirectoryId }] };
+		        var boundary = '2718281828459045';
+		        var body = [];
+		        body.push('--' + boundary);
+		        body.push('Content-Type: application/json');
+		        body.push('');
+		        body.push(JSON.stringify(metadata));
+		        body.push('');
+		        body.push('--' + boundary);
+		        // TODO(maxw): Use the correct content type.
+		        body.push('Content-Type: text/plain');
+		        body.push('');
+		        body.push(fileReader.result);
+		        body.push('');
+		        body.push('--' + boundary + '--');
+		        var bodyString = body.join('\r\n');
 
-            // Send a request to upload the file.
-            var xhr = new XMLHttpRequest();
-            xhr.onreadystatechange = function() {
-                if (xhr.readyState === 4) {
-                    if (xhr.status === 200) {
-                        console.log('File synced!');
-                        callback(fileEntry);
-                    } else {
-                        console.log('File failed to sync with status ' + xhr.status + '.');
-                    }
-                }
-            };
+		        // Send a request to upload the file.
+		        var xhr = new XMLHttpRequest();
+		        xhr.onreadystatechange = function() {
+		            if (xhr.readyState === 4) {
+		                if (xhr.status === 200) {
+		                    console.log('File synced!');
+		                    callback(fileEntry);
+		                } else {
+		                    console.log('File failed to sync with status ' + xhr.status + '.');
+		                }
+		            }
+		        };
 
-            xhr.open('POST', 'https://www.googleapis.com/upload/drive/v2/files?uploadType=multipart', true);
-            xhr.setRequestHeader('Content-Type', 'multipart/related; boundary=' + boundary);
-            xhr.setRequestHeader('Content-Length', bodyString.length);
-            xhr.setRequestHeader('Authorization', 'Bearer ' + _tokenString);
-            xhr.send(bodyString);
-        };
-        fileReader.readAsArrayBuffer(file);
-    };
+				// If there's a file id, update the file.  Otherwise, upload it anew.
+				if (fileId) {
+			        xhr.open('PUT', 'https://www.googleapis.com/upload/drive/v2/files/' + fileId + '?uploadType=multipart', true);
+		        } else {
+			        xhr.open('POST', 'https://www.googleapis.com/upload/drive/v2/files?uploadType=multipart', true);
+		        }
+		        xhr.setRequestHeader('Content-Type', 'multipart/related; boundary=' + boundary);
+		        xhr.setRequestHeader('Content-Length', bodyString.length);
+		        xhr.setRequestHeader('Authorization', 'Bearer ' + _tokenString);
+		        xhr.send(bodyString);
+		    };
+		    fileReader.readAsBinaryString(file);
+		};
 
+		// Get the file.
+		fileEntry.file(onFileSuccess);
+	};
     var onGetTokenStringSuccess = function(tokenString) {
         // Save the token string for later use.
         _tokenString = tokenString;
 
-        // Get the file.
-        fileEntry.file(onFileSuccess);
+        // Get the file id and pass it on.
+        getFileId(fileEntry, onGetFileIdSuccess);
     };
 
     getTokenString(onGetTokenStringSuccess);
@@ -146,16 +185,53 @@ function getSyncableParentDirectoryId(callback) {
         // Create a query that locates the desired directory.
         var query = 'mimeType = "application/vnd.google-apps.folder" and title = "Chrome Syncable FileSystem"';
 
-        // Send a request to upload the file.
+        // Send a request to locate the directory.
         var xhr = new XMLHttpRequest();
         xhr.onreadystatechange = function() {
             if (xhr.readyState === 4) {
                 if (xhr.status === 200) {
                     console.log('Successfully got directories.');
-
                     callback(JSON.parse(xhr.responseText).items[0].id);
                 } else {
                     console.log('Failed to get directories with status ' + xhr.status + '.');
+                }
+            }
+        };
+
+        xhr.open('GET', 'https://www.googleapis.com/drive/v2/files?q=' + query, true);
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.setRequestHeader('Authorization', 'Bearer ' + _tokenString);
+        xhr.send();
+    };
+
+    getTokenString(onGetTokenStringSuccess);
+}
+
+// This function retrieves the Drive file id of the given file, if it exists.  Otherwise, it yields null.
+function getFileId(fileEntry, callback) {
+    var onGetTokenStringSuccess = function(tokenString) {
+        // Save the token string for later use.
+        _tokenString = tokenString;
+
+        // Create a query that locates the desired file.
+        var query = 'title = "' + fileEntry.name + '"';
+
+        // Send a request to locate the file.
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
+                    console.log('Successfully searched for file.');
+                    var items = JSON.parse(xhr.responseText).items;
+                    if (items.length == 0) {
+                    	console.log('  No such file found.');
+                    	callback(null);
+                    } else {
+                    	console.log('  File found.');
+                    	callback(items[0].id);
+                    }
+                } else {
+                    console.log('Failed to search for file with status ' + xhr.status + '.');
                 }
             }
         };
