3093c90c794f59b36334f1c5ff7673949074c7b3
Ian Clelland <iclelland@chromium.org>
2013-05-27 10:48:35 -0400

    Fix cordova requires in releaseReadyWait method

--- plugins/chrome-bootstrap/api/helpers/ChromeExtensionURLs.ios.js
+++ plugins/chrome-bootstrap/api/helpers/ChromeExtensionURLs.ios.js
@@ -2,6 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+var exports = module.exports;
+var exec = cordova.require('cordova/exec');
+
 exports.releaseReadyWait = function() {
   cordova.exec(null,null,'ChromeExtensionURLs','release',[])
 };
