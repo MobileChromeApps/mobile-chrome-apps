df28fe846ae9a06374f14d6d805d665fe6a23596
Ian Clelland <iclelland@chromium.org>
2013-08-01 11:02:08 -0400

    Add Blob constructor polyfill to stanard plugin list

--- mca.js
+++ mca.js
@@ -83,7 +83,8 @@ var ACTIVE_PLUGINS = [
     path.join(scriptDir, 'chrome-cordova', 'plugins', 'chrome.storage'),
     path.join(scriptDir, 'chrome-cordova', 'plugins', 'polyfill-CustomEvent'),
     path.join(scriptDir, 'chrome-cordova', 'plugins', 'polyfill-Function.bind'),
-    path.join(scriptDir, 'chrome-cordova', 'plugins', 'polyfill-xhr-blob')
+    path.join(scriptDir, 'chrome-cordova', 'plugins', 'polyfill-xhr-blob'),
+    path.join(scriptDir, 'chrome-cordova', 'plugins', 'polyfill-blob-constructor')
 ];
 
 var PLUGIN_MAP = {
