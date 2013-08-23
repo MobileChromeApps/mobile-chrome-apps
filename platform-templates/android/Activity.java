6122a6d0b75a4a4e4fd250a3216906122c1d1641
Ian Clelland <iclelland@chromium.org>
2013-08-23 14:45:28 -0400

    Add new Android platform template to support CORS

---
+++ platform-templates/android/Activity.java
@@ -0,0 +1,42 @@
+/*
+       Licensed to the Apache Software Foundation (ASF) under one
+       or more contributor license agreements.  See the NOTICE file
+       distributed with this work for additional information
+       regarding copyright ownership.  The ASF licenses this file
+       to you under the Apache License, Version 2.0 (the
+       "License"); you may not use this file except in compliance
+       with the License.  You may obtain a copy of the License at
+
+         http://www.apache.org/licenses/LICENSE-2.0
+
+       Unless required by applicable law or agreed to in writing,
+       software distributed under the License is distributed on an
+       "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+       KIND, either express or implied.  See the License for the
+       specific language governing permissions and limitations
+       under the License.
+ */
+
+package __ID__;
+
+import android.os.Bundle;
+import org.apache.cordova.*;
+
+import com.google.cordova.ChromeChromeClient;
+
+public class __ACTIVITY__ extends DroidGap
+{
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        // Set by <content src="index.html" /> in config.xml
+        super.loadUrl(Config.getStartUrl());
+        //super.loadUrl("file:///android_asset/www/index.html")
+    }
+    
+    protected CordovaChromeClient makeChromeClient(CordovaWebView webView) {
+    	return new ChromeChromeClient(this, webView);
+    }
+}
+
