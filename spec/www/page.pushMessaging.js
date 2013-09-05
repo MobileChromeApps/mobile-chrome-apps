ed94cc075da444c8645b3b46a2253da3ab2c7080
Max Woghiren <max.woghiren@gmail.com>
2013-09-05 11:28:58 -0400

    Updated pushMessaging tests.

--- spec/www/page.pushMessaging.js
+++ spec/www/page.pushMessaging.js
@@ -2,18 +2,20 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// Chrome app GCM credentials
+// Chrome app GCM credentials.
 var clientId = '429153676186-efnn5o5otvpa75kpa82ee91qkd80evb3.apps.googleusercontent.com';
 var clientSecret = 'Q_Jk5GKsGcA-Dp9GcqMKRnAP';
 var refreshToken = '1/8SBtOiPJYTKAMWmYlmrMZ4Xk0B_Z3c_86peQETRCzjM';
 
-// Android GCM credentials
+// Android GCM credentials.
 var serverApiKey = 'AIzaSyC75_iRkHOwMm1nnMrP8iv6BZRIvzQvKDQ';
 
+// We store the channel and registration ids.
+var pushMessagingIds;
+
+// Add a listener.
 chrome.pushMessaging.onMessage.addListener(function(message) {
-  chromespec.log('push message arrived, round trip complete!');
-  chromespec.log('subchannel - ' + message.subchannelId);
-  chromespec.log('payload - ' + message.payload);
+  chromespec.log('Push message arrived. Subchannel: ' + message.subchannelId + ', message: "' + JSON.parse(message.payload).message + '"');
 });
 
 chromespec.registerSubPage('chrome.pushMessaging', function(rootEl) {
@@ -22,67 +24,71 @@ chromespec.registerSubPage('chrome.pushMessaging', function(rootEl) {
     rootEl.appendChild(button);
   }
 
-  function getAccessToken(callback) {
-    var req = new XMLHttpRequest();
-    req.open('POST', 'https://accounts.google.com/o/oauth2/token', true);
-    req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-    req.onreadystatechange = function() {
-      if (req.readyState == 4) {
-        if (req.status == 200) {
-          var response = JSON.parse(req.responseText);
-          callback(response.access_token);
-        } else {
-          console.error('Access token request failed with status ' + req.status);
+  addButton('Get Channel Id', function() {
+    var onGetChannelIdSuccess = function(channelId) {
+      pushMessagingIds = channelId;
+      chromespec.log('Channel id: ' + pushMessagingIds.channelId);
+      chromespec.log('Registration id: ' + pushMessagingIds.registrationId);
+    };
+
+    chrome.pushMessaging.getChannelId(true /* interactive */, onGetChannelIdSuccess);
+  });
+
+  addButton('Send Push Message via Android', function() {
+    if (!pushMessagingIds || !pushMessagingIds.registrationId) {
+      chromespec.log('No registration id!');
+      return;
+    }
+
+    chromespec.log('Sending push message...');
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', 'https://android.googleapis.com/gcm/send');
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.setRequestHeader('Authorization', 'key=' + serverApiKey);
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState == 4) {
+        if (xhr.status < 200 || xhr.status > 300) {
+          chromespec.log('Android GCM push message request failed with status ' + xhr.status + '.');
         }
       }
     }
-    var data = 'client_id=' + clientId + '&' +
-               'client_secret=' + clientSecret + '&' +
-               'refresh_token=' + refreshToken + '&' +
-               'grant_type=refresh_token';
-    req.send(data);
-  }
+    var data = {'registration_ids' : [ pushMessagingIds.registrationId ], 'data' : { 'message' : 'Push message sent via Android GCM.' } };
+    xhr.send(JSON.stringify(data));
 
-  function sendPushMessage(channelId, subchannelId, payload) {
-    if ('channelId' in channelId) {
-      // Chrome app GCM request.
-      getAccessToken(function(token) {
-        var req = new XMLHttpRequest();
-        req.open('POST', 'https://www.googleapis.com/gcm_for_chrome/v1/messages?access_token=' + token, true);
-        req.setRequestHeader('Content-Type', 'application/json');
-        req.onreadystatechange = function() {
-          if (req.readyState == 4) {
-            if (req.status < 200 || req.status > 300) {
-              console.error('Chrome app GCM push message request failed with status ' + req.status);
-            }
-          }
-        }
-        var data = { 'channelId':channelId.channelId, 'subchannelId':subchannelId, 'payload':JSON.stringify(payload) };
-        req.send(JSON.stringify(data));
-      });
+    chromespec.log('...push message sent.');
+  });
+
+  addButton('Send Push Message via Chrome', function() {
+    if (!pushMessagingIds || !pushMessagingIds.channelId) {
+      chromespec.log('No channel id!');
+      return;
     }
-    
-    if ('registrationId' in channelId) {
-      // Android GCM request.
-      var req = new XMLHttpRequest();
-      req.open('POST', 'https://android.googleapis.com/gcm/send', true);
-      req.setRequestHeader('Content-Type', 'application/json');
-      req.setRequestHeader('Authorization', 'key=' + serverApiKey);
-      req.onreadystatechange = function() {
-        if (req.readyState == 4) {
-          if (req.status < 200 || req.status > 300) {
-            console.error('Android GCM push message request failed with status ' + req.status);
+
+    chromespec.log('Sending push message...');
+
+    var getAuthTokenCallback = function(token) {
+      var xhr = new XMLHttpRequest();
+      xhr.open('POST', 'https://www.googleapis.com/gcm_for_chrome/v1/messages');
+      xhr.setRequestHeader('Content-Type', 'application/json');
+      xhr.setRequestHeader('Authorization', 'Bearer ' + token);
+      xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4) {
+          if (xhr.status < 200 || xhr.status > 300) {
+            chromespec.log('Chrome GCM push message request failed with status ' + xhr.status + '.');
+            chromespec.log('Response text: ' + xhr.responseText);
           }
         }
       }
-      var data = {'registration_ids':[channelId.registrationId], 'data':payload };
-      req.send(JSON.stringify(data));
-    }
-  }
+      var data = { 'channelId' : pushMessagingIds.channelId,
+                   'subchannelId' : 0,
+                   'payload' : JSON.stringify({ 'message' : 'Push message sent via Chrome GCM.' }) };
+      xhr.send(JSON.stringify(data));
 
-  addButton('Push Messaging Roundtrip', function() {
-    chrome.pushMessaging.getChannelId(true, function(channelId) {
-      sendPushMessage(channelId, '0', { message:'Hello World!' });
-    });
+      chromespec.log('...push message sent.');
+    };
+
+    chrome.identity.getAuthToken({ interactive: true }, getAuthTokenCallback);
   });
 });
+
