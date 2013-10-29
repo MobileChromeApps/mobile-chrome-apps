ba4d5392680d7669b813e06f1608f4f166773364
Max Woghiren <max.woghiren@gmail.com>
2013-10-29 17:22:21 -0400

    Added chrome.idle.

---
+++ plugins/chrome.idle/idle.js
@@ -0,0 +1,73 @@
+// Copyright (c) 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+var exec = require('cordova/exec');
+
+var STATE_ACTIVE = 'active';
+var STATE_IDLE = 'idle';
+var STATE_LOCKED = 'locked';
+
+// This keeps track of the current state.
+var currentState = STATE_ACTIVE;
+
+// These listeners are called when the device's state changes.
+var stateListeners = [ ];
+
+// This interval represents the amount of idle time required for the above state listeners to fire with status "idle".
+var detectionIntervalInSeconds = 60;
+
+// This is a timeout used to trigger the idle state.
+var idleTimer = null;
+
+exports.queryState = function(_detectionIntervalInSeconds, callback) {
+    // TODO(maxw): Implement this!
+};
+
+exports.setDetectionInterval = function(_detectionIntervalInSeconds) {
+    detectionIntervalInSeconds = _detectionIntervalInSeconds;
+    resetIdleTimer();
+};
+
+exports.onStateChanged = { };
+exports.onStateChanged.addListener = function(listener) {
+    if (typeof(listener) === 'function') {
+        stateListeners.push(listener);
+    } else {
+        console.log('Attempted to add a non-function listener.');
+    }
+}
+
+// This function fires the state listeners with the given state.
+var fireListeners = function(state) {
+    for (var i = 0; i < stateListeners.length; i++) {
+        stateListeners[i](state);
+    }
+};
+
+// This function resets the idle timer.
+var resetIdleTimer = function() {
+    clearTimeout(idleTimer);
+    idleTimer = setTimeout(changeState, detectionIntervalInSeconds * 1000, STATE_IDLE);
+};
+
+// This function handles a change in state.
+var changeState = function(state) {
+    // If we have a new state, set it appropriately and fire the state listeners.
+    if (currentState !== state) {
+        currentState = state;
+        fireListeners(state);
+    }
+}
+
+// This function handles a touch event by resetting the idle timer and changing the state.
+var handleTouchEvent = function() {
+    resetIdleTimer();
+    changeState(STATE_ACTIVE);
+}
+
+// Start the idle timer.
+resetIdleTimer();
+
+// Add a touch listener.
+document.body.addEventListener('touchstart', handleTouchEvent);
---
+++ spec/www/page.idle.js
@@ -0,0 +1,36 @@
+// Copyright (c) 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+chromespec.registerSubPage('chrome.idle', function(rootEl) {
+  function addButton(name, func) {
+    var button = chromespec.createButton(name, func);
+    rootEl.appendChild(button);
+  }
+
+  addButton('Query state', function() {
+    var detectionIntervalInSeconds = 10;
+    var queryStateCallback = function(state) {
+      chromespec.log('State: ' + state);
+    };
+    chrome.idle.queryState(detectionIntervalInSeconds, queryStateCallback);
+  });
+
+  // TODO(maxw): Allow the detection interval to be set in a textbox.
+  addButton('Change detection interval to 10 seconds', function() {
+    chrome.idle.setDetectionInterval(10);
+    chromespec.log('Detection interval set to 10 seconds.');
+  });
+
+  addButton('Change detection interval to 60 seconds', function() {
+    chrome.idle.setDetectionInterval(60);
+    chromespec.log('Detection interval set to 60 seconds.');
+  });
+
+  // Add a status-change listener.
+  var stateListener = function(state) {
+    chromespec.log('State changed: ' + state);
+  };
+  chrome.idle.onStateChanged.addListener(stateListener);
+});
+
