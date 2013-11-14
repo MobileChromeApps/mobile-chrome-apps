54f56bb1e4b35e84ed9362f61979d52649593ee4
Max Woghiren <max.woghiren@gmail.com>
2013-11-14 13:09:31 -0500

    Added a method to AppDelegate programmatically.
    This removes an annoying manual step from the installation of the chrome.identity plugin.

--- plugins/chrome.identity/README.md
+++ plugins/chrome.identity/README.md
@@ -38,21 +38,6 @@ For iOS, first follow the instructions [here](https://developers.google.com/+/mo
 
 Next, in Xcode, click on the project in the left sidebar and navigate to the `Build Phases` tab.  In the `Compile Sources` section, remove `OpenInChromeController.m`.
 
-Finally, you'll need to add a little bit of code to your app delegate.  First, an import:
-
-    #import <GooglePlus/GooglePlus.h>
-
-Then, the following message:
-
-    - (BOOL)application: (UIApplication *)application
-                openURL: (NSURL *)url
-      sourceApplication: (NSString *)sourceApplication
-             annotation: (id)annotation {
-        return [GPPURLHandler handleURL:url
-                      sourceApplication:sourceApplication
-                             annotation:annotation];
-    }
-
 ## Updating Your Manifest
 
 Your manifest needs to be updated to include your client id and scopes.  In a Mobile Chrome App (ie. an app created using mca.js), this is done in manifest.json as follows:
--- plugins/chrome.identity/src/ios/ChromeIdentity.m
+++ plugins/chrome.identity/src/ios/ChromeIdentity.m
@@ -4,6 +4,7 @@
 
 #import <GoogleOpenSource/GoogleOpenSource.h>
 #import <GooglePlus/GooglePlus.h>
+#import "AppDelegate.h"
 #import "ChromeIdentity.h"
 
 #if CHROME_IDENTITY_VERBOSE_LOGGING
@@ -11,6 +12,28 @@
 #else
 #define VERBOSE_LOG(args...) do {} while (false)
 #endif
+
+@interface AppDelegate (IdentityUrlHandling)
+
+- (BOOL)application: (UIApplication *)application
+            openURL: (NSURL *)url
+  sourceApplication: (NSString *)sourceApplication
+         annotation: (id)annotation;
+
+@end
+
+@implementation AppDelegate (IdentityUrlHandling)
+
+- (BOOL)application: (UIApplication *)application
+            openURL: (NSURL *)url
+  sourceApplication: (NSString *)sourceApplication
+         annotation: (id)annotation {
+    return [GPPURLHandler handleURL:url
+                  sourceApplication:sourceApplication
+                         annotation:annotation];
+}
+
+@end
 
 @implementation ChromeIdentity
 
@@ -21,6 +44,9 @@
     GPPSignIn *signIn = [GPPSignIn sharedInstance];
     [signIn setShouldFetchGooglePlusUser:YES];
     [signIn setDelegate:self];
+
+    // Add a necessary method to AppDelegate.
+    swizzleMethod([AppDelegate class], @selector(application:openURL:sourceApplication:annotation:), @selector(application:openURL:sourceApplication:annotation:));
 
     return self;
 }
@@ -66,6 +92,21 @@
 
     // Clear the callback id.
     [self setCallbackId:nil];
+}
+
+#pragma mark Swizzling
+
+void swizzleMethod(Class class, SEL destinationSelector, SEL sourceSelector)
+{
+    Method destinationMethod = class_getInstanceMethod(class, destinationSelector);
+    Method sourceMethod = class_getInstanceMethod(class, sourceSelector);
+
+    // If the method doesn't exist, add it.  If it does exist, replace it with the given implementation.
+    if (class_addMethod(class, destinationSelector, method_getImplementation(sourceMethod), method_getTypeEncoding(sourceMethod))) {
+        class_replaceMethod(class, destinationSelector, method_getImplementation(destinationMethod), method_getTypeEncoding(destinationMethod));
+    } else {
+        method_exchangeImplementations(destinationMethod, sourceMethod);
+    }
 }
 
 @end
