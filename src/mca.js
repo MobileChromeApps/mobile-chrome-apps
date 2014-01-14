f66abafcd77f8079e1242faa6f68b51418b611e0
Andrew Grieve <agrieve@chromium.org>
2014-01-13 23:40:51 -0500

    Make mca.js use a normal hashbang to fix cygwin usage

--- src/mca.js
+++ src/mca.js
@@ -1,4 +1,4 @@
-//usr/bin/env node $0 $*; exit $?
+#!/usr/bin/env node
 /**
   Licensed to the Apache Software Foundation (ASF) under one
   or more contributor license agreements.  See the NOTICE file
