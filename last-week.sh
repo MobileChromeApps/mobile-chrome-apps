16ef99d72b56652c9f32b9e17bf24fc5fdb3a1a7
Michal Mocny <mmocny@gmail.com>
2014-01-20 10:53:16 -0500

    Adding short-hash to last-week command format

--- last-week.sh
+++ last-week.sh
@@ -8,11 +8,11 @@ git fetch origin
   cd mobile-chrome-app-samples
   git fetch origin
 )
-git log --no-merges --date=short --all-match --fixed-strings --committer="$EMAIL" --author="$EMAIL" --format="mobile-chrome-apps %s" --since="7 days ago" origin/master
+git log --no-merges --date=short --all-match --fixed-strings --committer="$EMAIL" --author="$EMAIL" --format="mobile-chrome-apps %h %s" --since="7 days ago" origin/master
 (
   cd chrome-cordova
-  git log --no-merges --date=short --all-match --fixed-strings --committer="$EMAIL" --author="$EMAIL" --format="chrome-cordova %s" --since="7 days ago" origin/master
+  git log --no-merges --date=short --all-match --fixed-strings --committer="$EMAIL" --author="$EMAIL" --format="chrome-cordova %h %s" --since="7 days ago" origin/master
 ); (
   cd mobile-chrome-app-samples
-  git log --no-merges --date=short --all-match --fixed-strings --committer="$EMAIL" --author="$EMAIL" --format="mobile-chrome-app-samples %s" --since="7 days ago" origin/master
+  git log --no-merges --date=short --all-match --fixed-strings --committer="$EMAIL" --author="$EMAIL" --format="mobile-chrome-app-samples %h %s" --since="7 days ago" origin/master
 )
