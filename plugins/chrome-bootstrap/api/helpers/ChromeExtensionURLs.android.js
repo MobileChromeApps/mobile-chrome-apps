e6a888c9c57c768104b4e86a5353cf3ffe5f6ea1
Andrew Grieve <agrieve@chromium.org>
2014-02-04 16:57:52 -0500

    Fire document readystatechange events on Android (fixes Polymer on KitKat)

--- plugins/chrome-bootstrap/api/helpers/ChromeExtensionURLs.android.js
+++ plugins/chrome-bootstrap/api/helpers/ChromeExtensionURLs.android.js
@@ -4,8 +4,10 @@
 
 exports.releaseReadyWait = function() {
   Object.defineProperty(document, 'readyState', {get: function() { return 'interactive'}, configurable: true });
+  cordova.fireDocumentEvent('readystatechange');
   cordova.fireDocumentEvent('DOMContentLoaded');
   cordova.fireWindowEvent('DOMContentLoaded');
   Object.defineProperty(document, 'readyState', {get: function() { return 'complete'}, configurable: true });
+  cordova.fireDocumentEvent('readystatechange');
   cordova.fireWindowEvent('load');
 };
