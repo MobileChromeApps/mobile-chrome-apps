ac4e76750a5b37c3048cc3c7262cf4ba81574574
Michal Mocny <mmocny@gmail.com>
2014-02-06 15:07:14 -0500

    Removing jasmine tests

--- spec/www/background.js
+++ spec/www/background.js
@@ -1,15 +1,3 @@
 // Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-
-chromespec.registerJasmineTest('chrome.alarms');
-chromespec.registerJasmineTest('chrome.app.runtime');
-chromespec.registerJasmineTest('chrome.app.window');
-chromespec.registerJasmineTest('chrome.fileSystem');
-chromespec.registerJasmineTest('chrome.i18n');
-chromespec.registerJasmineTest('chrome.notifications');
-chromespec.registerJasmineTest('chrome.runtime');
-chromespec.registerJasmineTest('chrome.storage');
-chromespec.registerJasmineTest('chrome.socket');
-chromespec.registerJasmineTest('pageload');
-chromespec.registerJasmineTest('xhr');
