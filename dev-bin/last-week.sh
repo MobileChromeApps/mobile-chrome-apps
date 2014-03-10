ed317112c61ed52464c8905840dcfce7d06a4ec3
Andrew Grieve <agrieve@chromium.org>
2014-03-10 10:52:16 -0400

    Remove custom chrome-cordova logic from last-week.sh

--- dev-bin/last-week.sh
+++ dev-bin/last-week.sh
@@ -1,12 +1,4 @@
 #!/bin/sh
 EMAIL=$(git config user.email)
 git fetch origin
-(
-  cd chrome-cordova
-  git fetch origin
-)
 git log --no-merges --date=short --all-match --fixed-strings --committer="$EMAIL" --author="$EMAIL" --format="mobile-chrome-apps %h %s" --since="7 days ago" origin/master
-(
-  cd chrome-cordova
-  git log --no-merges --date=short --all-match --fixed-strings --committer="$EMAIL" --author="$EMAIL" --format="chrome-cordova %h %s" --since="7 days ago" origin/master
-)
