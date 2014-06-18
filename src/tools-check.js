9cc2c497b2d5e370c0f0c84a4cb33707c29be1da
Michal Mocny <mmocny@gmail.com>
2014-06-18 10:46:27 -0400

    Cache results of tools-check

--- src/tools-check.js
+++ src/tools-check.js
@@ -1,7 +1,10 @@
-var os = require('os');
 var Q = require('q');
 
 var utils = require('./utils');
+
+var hasAndroidSdk;
+var hasAndroidPlatform;
+var hasXcode;
 
 function parseTargetOutput(targetOutput) {
   var targets = [];
@@ -13,17 +16,7 @@ function parseTargetOutput(targetOutput) {
   return targets;
 }
 
-// Returns a promise.
-module.exports = exports = function toolsCheck() {
-  console.log('## Checking that tools are installed');
-
-  var hasAndroidSdk = false;
-  var hasAndroidPlatform = false;
-  var hasXcode = false;
-
-  if (!os.tmpdir) return Q.reject('Your version of node (' + process.version + ') is too old. Please update your version of node.');
-
-  // Android
+function checkHasAndroid() {
   return utils.exec('android list targets', true /* opt_silent */).then(function(out) {
     var targetOutput = out.stdout;
     hasAndroidSdk = true;
@@ -35,10 +28,13 @@ module.exports = exports = function toolsCheck() {
         '\nAdd it using the Android SDK Manager (run the "android" command)');
     }
     // Stacking up here because we want to bail after the first one fails.
-    return utils.exec('ant -version', true /* opt_silent */).then(function() {
+    return utils.exec('ant -version', true /* opt_silent */)
+    // TODO: Should we replace the inline error handlers with .fail()/.catch() calls, so error handler code follow calls directly?
+    .then(function() {
       // Project creation does succeed without javac.
       hasAndroidPlatform = true;
-      return utils.exec('javac -version', true /* opt_silent */).then(null, function(err) {
+      return utils.exec('javac -version', true /* opt_silent */)
+      .then(null, function(err) {
         console.warn('Dependency warning: `javac` command not detected on your PATH.');
       });
     }, function(err) {
@@ -47,31 +43,48 @@ module.exports = exports = function toolsCheck() {
   }, function(err) {
     console.warn('Android not detected (`android` command not detected on your PATH).');
   })
-  // iOS
+}
+
+function checkHasIos() {
+  if (process.platform != 'darwin')
+    return Q();
+
+  return utils.exec('which xcodebuild', true /* opt_silent */)
   .then(function() {
-    if (process.platform != 'darwin') return;
-    return utils.exec('which xcodebuild', true /* opt_silent */).then(function() {
-      return utils.exec('xcodebuild -version', true /* opt_silent */).then(function() {
-        hasXcode = true;
-        console.log('Xcode detected.');
-      }, function(err) {
-        console.log('Xcode appears to be installed, but no version is selected (fix this with xcodeselect).');
-      });
+    return utils.exec('xcodebuild -version', true /* opt_silent */)
+    .then(function() {
+      hasXcode = true;
+      console.log('Xcode detected.');
     }, function(err) {
-      console.log('Xcode not detected.');
+      console.log('Xcode appears to be installed, but no version is selected (fix this with xcodeselect).');
     });
-  })
-  // Check for at least one of the tools.
-  .then(function() {
-    if (!hasAndroidPlatform && ! hasXcode) {
+  }, function(err) {
+    console.log('Xcode not detected.');
+  });
+}
+
+// Returns a promise.
+module.exports = exports = function toolsCheck() {
+  var ret = Q();
+
+  // Is this the first time we're checking for the tools?
+  if (typeof hasAndroidSdk == 'undefined') {
+    ret = ret.then(function() {
+      console.log('## Checking that tools are installed');
+      return checkHasAndroid().then(checkHasIos);
+    });
+  }
+
+  return ret.then(function() {
+    if (!hasAndroidPlatform && !hasXcode)
+      // TODO: Replace inline docs links with general instructions to read --help / docs / report issues
       return Q.reject('No usable build environment could be found. Please refer to our installation guide:\n' +
           'https://github.com/MobileChromeApps/mobile-chrome-apps/blob/master/docs/Installation.md');
-    } else {
-      return Q({
-        hasAndroidSdk: hasAndroidSdk,
-        hasAndroidPlatform: hasAndroidPlatform,
-        hasXcode: hasXcode,
-      });
-    }
+
+    return Q({
+      hasAndroidSdk: hasAndroidSdk,
+      hasAndroidPlatform: hasAndroidPlatform,
+      hasXcode: hasXcode,
+    });
   });
 };
