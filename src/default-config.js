2535abbe0bc9f2c03832f4c329062d4caae422b1
Mark Koudritsky <kamrik@gmail.com>
2014-09-09 10:39:00 -0400

    Don't add local chrome plugins dir if it doesn't exit
    If plugin search path contains a non existing directory, cordova lib fails.
    This was fixed for the current master version of cordova-lib but is still a
    problem in the latest published version.

--- src/default-config.js
+++ src/default-config.js
@@ -1,12 +1,21 @@
 var path = require('path');
+var fs = require('fs');
 
 module.exports = exports = function defaultConfig(ccaRoot) {
+
+  var plugin_search_path = [
+    path.join(ccaRoot, 'cordova'),
+    path.join(ccaRoot, 'cordova', 'cordova-plugins'),
+  ];
+
+  // For plugin development with local cca git checkout.
+  var chrome_cordova_plugins = path.join(ccaRoot, 'chrome-cordova', 'plugins');
+  if (fs.existsSync(chrome_cordova_plugins)) {
+    plugin_search_path.push(chrome_cordova_plugins);
+  }
+
   return {
-    plugin_search_path: [
-        path.join(ccaRoot, 'cordova'),
-        path.join(ccaRoot, 'cordova', 'cordova-plugins'),
-        path.join(ccaRoot, 'chrome-cordova', 'plugins'),  // For plugin development with local cca git checkout.
-        ],
+    plugin_search_path: plugin_search_path,
     lib: {
       android: {
         // TODO: Remove uri here and below after cordova-lib@0.21.7 is released.
