375772276dbf800cdcf9e5e05367b7d45e902889
Andrew Grieve <agrieve@chromium.org>
2015-01-09 11:58:43 -0500

    Add --ios and --android flags to create-spec.sh

--- dev-bin/create-spec.sh
+++ dev-bin/create-spec.sh
@@ -5,16 +5,42 @@ if [[ "$0" = /* ]]; then
 else
   SCRIPT_PATH="$PWD/$(dirname $0)"
 fi
-DIR_NAME=${1-ChromeSpec}
 CCA_PATH="$SCRIPT_PATH/../src/cca.js"
 SPEC_PATH="$SCRIPT_PATH/../chrome-cordova/chrome-apps-api-tests"
+ANDROID=1
+IOS=0
+
+if [[ "$1" = "--ios" || "$2" = "--ios" ]]; then
+  IOS=1
+  ANDROID=0
+fi
+if [[ "$1" = "--android" || "$2" = "--android" ]]; then
+  ANDROID=1
+fi
+
+if [[ "$1" != -* ]]; then
+  DIR_NAME=${1-ChromeSpec}
+elif [[ "$2" != -* ]]; then
+  DIR_NAME=${2-ChromeSpec}
+elif [[ "$3" != -* ]]; then
+  DIR_NAME=${3-ChromeSpec}
+fi
+
 echo Creating spec at: $DIR_NAME
 set -e
-set -x
 rm -rf $DIR_NAME
 $CCA_PATH create $DIR_NAME --link-to=${SPEC_PATH}/www
-cp $SPEC_PATH/debug-key.p12 $DIR_NAME
-cp $SPEC_PATH/android-debug-keys.properties $DIR_NAME
-cd $DIR_NAME
-$CCA_PATH build android --link
-cp $SPEC_PATH/build-extras.gradle platforms/android
+if (( $IOS )); then
+  set -x
+  (cd $DIR_NAME && $CCA_PATH build ios --link)
+  set +x
+fi
+if (( $ANDROID )); then
+  set -x
+  cp $SPEC_PATH/debug-key.p12 $DIR_NAME
+  cp $SPEC_PATH/android-debug-keys.properties $DIR_NAME
+  cd $DIR_NAME
+  $CCA_PATH build android --link
+  cp $SPEC_PATH/build-extras.gradle platforms/android
+  set +x
+fi
