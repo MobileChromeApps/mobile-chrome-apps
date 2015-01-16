73b85bf005d574ee484a1924c1c42597bce960b0
Andrew Grieve <agrieve@chromium.org>
2015-01-16 14:39:05 -0500

    Change helpers.delayDeviceReadyUntil() and helpers.queueLifeCycleEvent()
    helpers.delayDeviceReadyUntil(): To avoid each plugin needing to deal with directly with Cordova channels.
    helpers.queueLifeCycleEvent(): Make it work better in the
        vanilla-cordova case, and have it call the callback immediately if
        lifecycle events have already fired.

--- chrome-cordova/plugins/chrome-common/helpers.js
+++ chrome-cordova/plugins/chrome-common/helpers.js
@@ -4,21 +4,56 @@
 
 // Distinguish between having the plugin loaded vs. being run using chromeapp.html.
 var isChromeApp = /chromeapp.html$/.exec(location.href);
+var channel = require('cordova/channel');
 
+// TODO: Delete this method if favour of delayDeviceReadyUntil.
 exports.runAtStartUp = function(func) {
   if (isChromeApp) {
+    // TODO: Implement done() call back for chrome app case.
     var mobile = require('org.chromium.bootstrap.mobile.impl');
     mobile.onBackgroundPageLoaded.subscribe(func);
   } else {
-    document.addEventListener('deviceready', func, false);
+    channel.onCordovaReady.subscribe(func);
   }
 };
 
+// TODO: Add this to cordova-js.
+// Executes func after onCordovaReady fires, and delays deviceready until done() is called.
+exports.delayDeviceReadyUntil = function(func) {
+  channel.onCordovaReady.subscribe(function() {
+    var newChannel = channel.createSticky('cca-delay-device-ready');
+    channel.deviceReadyChannelsArray.push(newChannel);
+    func(function done() {
+      newChannel.fire();
+    });
+  });
+};
+
+// For Chrome Apps:
+//  - Call func after background page is loaded.
+//  - Prevents onLaunched from being fired
+//  - func() called immeditately if start-up sequence has already completed.
+// For vanilla Cordova:
+//  - Call func after deviceready has been fired.
+//  - func() called immeditately if deviceready has already fired.
 exports.queueLifeCycleEvent = function(func) {
   if (isChromeApp) {
     var mobile = require('org.chromium.bootstrap.mobile.impl');
-    mobile.lifeCycleEventFuncs.push(func);
+    if (mobile.lifeCycleEventFuncs) {
+      mobile.lifeCycleEventFuncs.push(func);
+    } else {
+      // Initial events have already occurred.
+      func();
+    }
   } else {
-    document.addEventListener('deviceready', func, false);
+    if (channel.onDeviceReady.state == 1) {
+      // Fire after deviceready, and within a setTimeout to allow
+      // user code to register listeners.
+      channel.onDeviceReady.subscribe(function() {
+        setTimeout(func, 0);
+      });
+    } else {
+      func();
+    }
   }
 };
