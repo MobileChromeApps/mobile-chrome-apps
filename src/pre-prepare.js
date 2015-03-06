46e34dd0b25cfa3e81abbec8bea423fd768dbda8
Andrew Grieve <agrieve@chromium.org>
2015-03-06 13:49:37 -0500

    Pin major version of bootstrap, i18n, and navigation plugins
    This will allow us to make non-backwards-compatible changes without
    breaking older cca clients

--- src/pre-prepare.js
+++ src/pre-prepare.js
@@ -130,6 +130,19 @@ module.exports = exports = function prePrepareCommand(context) {
       };
       var cmds = [];
       if (missingPlugins.length) {
+        // Pin major versions of plugins that are tied to cca hooks
+        var idx = missingPlugins.indexOf('org.chromium.navigation');
+        if (idx != -1) {
+          missingPlugins[idx] = missingPlugins[idx] + '@1';
+        }
+        idx = missingPlugins.indexOf('org.chromium.i18n');
+        if (idx != -1) {
+          missingPlugins[idx] = missingPlugins[idx] + '@1';
+        }
+        idx = missingPlugins.indexOf('org.chromium.bootstrap');
+        if (idx != -1) {
+          missingPlugins[idx] = missingPlugins[idx] + '@2';
+        }
         cmds.push(['plugin', 'add', missingPlugins, opts]);
       }
       if (excessPlugins.length) {
