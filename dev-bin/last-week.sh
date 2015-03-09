e81afe5c3e95ef1c07e68e54a7d3423cacb016b3
Andrew Grieve <agrieve@chromium.org>
2015-03-09 10:59:52 -0400

    Make last-week.sh list plugin changes as well

--- dev-bin/last-week.sh
+++ dev-bin/last-week.sh
@@ -6,3 +6,6 @@ CADT=../chrome-app-developer-tool
 if [[ -e "$CADT" ]]; then
   ( cd "$CADT" && git log --no-merges --date=short --all-match --fixed-strings --committer="$EMAIL" --author="$EMAIL" --format="chrome-app-developer-tool %h %s" --since="7 days ago" origin/master )
 fi
+for d in ../mobile-chrome-apps-plugins/*; do
+  ( cd $d; git log --no-merges --date=short --all-match --fixed-strings --committer="$EMAIL" --author="$EMAIL" --format="$(basename $d) %h %s" --since="7 days ago" origin/master )
+done
